8a46c43804ba943b8eb992889dfc5b70c14e7efe
githubtodj <shuyue121>
2017-06-11 01:01:00 +0800

    修改新增syso444

--- tt/src/com/util/StringUtil.java
+++ tt/src/com/util/StringUtil.java
@@ -2,7 +2,6 @@ package com.util;
 
 public class StringUtil {
 	public static void main(String[] args) {
-		System.out.println("11111111");
-		System.out.println("2222222222");
+		System.out.println("44444444");
 	}
 }
