6d42256c040b97c810e2e46f1583a1e275d89069
githubtodj <shuyue121>
2017-06-11 01:27:49 +0800

    修改6666

--- tt/src/com/util/StringUtil.java
+++ tt/src/com/util/StringUtil.java
@@ -2,9 +2,6 @@ package com.util;
 
 public class StringUtil {
 	public static void main(String[] args) {
-		System.out.println("5555555");
-		System.out.println("3333");
-		System.out.println("3333");
-		System.out.println("3333");
+		System.out.println("6666");
 	}
 }
